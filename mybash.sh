668859cb476b6f857475cbd3abe7ec53145de7a1
Zahed <zahedm45@gmail.com>
2025-01-09 14:29:24 +0100

    Temp

--- mybash.sh
+++ mybash.sh
@@ -2,7 +2,9 @@
 module load gprofng
 
 
-SIZES="20 40 60 80 100 200 400 600 800 1000 1200 1400 1600"
+#SIZES="20 40 60 80 100 200 400 600 800 1000 1200 1400 1600"
+
+SIZES="100"
 FUNCTIONS="mnk mkn nmk nkm kmn knm"
 
 for F in $FUNCTIONS; do
@@ -15,5 +17,6 @@ for F in $FUNCTIONS; do
     FILE_NAME="${NAME}_${S}.er"
     gprofng collect app -o "$FILE_NAME" -h dch,on,dcm,on,l2h,on,l2m,on,l3h,on,l3m,on ./matmult_c.gcc "$F" $S $S $S
     gprofng display text -functions "$FILE_NAME" >> "$TEXT_F_NAME"
+
   done
 done
