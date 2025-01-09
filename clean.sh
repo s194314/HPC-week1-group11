2420bdacb9e3cc28b064cce57683bb71d725533a
Zahed <zahedm45@gmail.com>
2025-01-09 17:37:51 +0100

    Added bash to clean up generated files

---
+++ clean.sh
@@ -0,0 +1,3 @@
+
+rm -rf *.er
+rm -rf *.text
--- mybash.sh
+++ mybash.sh
@@ -12,10 +12,16 @@ for F in $FUNCTIONS; do
   TEXT_F_NAME="${NAME}.text"
   rm -rf "$TEXT_F_NAME"
 
+
+  for (( i = 0; i < 5; i++ )); do
+
+  done
+
   for S in $SIZES; do
     echo -e "Size: {$S}\n" >> "$TEXT_F_NAME"
     FILE_NAME="${NAME}_${S}.er"
     gprofng collect app -o "$FILE_NAME" -h dch,on,dcm,on,l2h,on,l2m,on,l3h,on,l3m,on ./matmult_c.gcc "$F" $S $S $S
+    C1H=$(gprofng display text -functions file3.er | grep "<Total>" | awk '{print $5}')
     gprofng display text -functions "$FILE_NAME" >> "$TEXT_F_NAME"
 
   done
